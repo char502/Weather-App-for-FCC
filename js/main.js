2dd5909a1c4e843122fdadf35df800f75e33d4ad
Charlotte Ellwood <char502@hotmail.com>
2018-04-19 15:08:55 +0100

    html and css sections mostly finished and added a gitignore file

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
+$(document).ready(function () {
+    //$(small).hide();
+    if (navigator.geolocation) {
+        var currentPosition = '';
+        navigator.geolocation.getCurrentPosition(function (position) {
+            currentPosition = position;
 
+            var url = "https://fcc-weather-api.glitch.me/api/current?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude
 
-
-
+            $.getJSON(url, function (data) {
+                console.log(data);
+            });
+        });
+    }
+});
 
