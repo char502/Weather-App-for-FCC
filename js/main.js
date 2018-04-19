988e9e74ad21759346d02c373806b8eeb514e99e
Charlotte Ellwood <char502@hotmail.com>
2018-04-19 21:09:49 +0100

    getting the spi working

--- js/main.js
+++ js/main.js
@@ -8,7 +8,16 @@ $(document).ready(function () {
             var url = "https://fcc-weather-api.glitch.me/api/current?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude
 
             $.getJSON(url, function (data) {
+                //console.log(data);
+
+                var data = JSON.stringify(data);
+                var json = JSON.parse(data);
+
                 console.log(data);
+                console.log(json);
+
+                var country = json.name;
+                console.log(country);
             });
         });
     }
